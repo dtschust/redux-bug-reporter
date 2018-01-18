2db48092355495008ac6571b1512a03028679c98
Brad Barwick <bbarwick@fandango.com>
2018-01-18 09:02:26 -0800

    Config Jest for Babel/JSX.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
 	testMatch: [
 		'<rootDir>/test/**/*.js'
 	],
+	"transform": {
+		"^.+\\.js$": "babel-jest",
+		"^.+\\.jsx$": "babel-jest"
+	},
 	"setupFiles": [
 		"raf/polyfill"
 	],
